a37c601a6c45e11114f0f27b4459127ddddbd826
lingxueli <ll2871@columbia.edu>
2018-10-04 22:08:30 -0600

    Adjust to Hapi API change in new version

--- index.js
+++ index.js
@@ -1,18 +1,67 @@
 var MongoClient = require('mongodb').MongoClient;
 var url = 'mongodb://localhost:27017';
 
-var findDocuments = function(db, callback){
-    var collection = db.collection('tours');
+var Hapi = require('hapi');
+var server = new Hapi.Server({ port: 8080, host: 'localhost'});
 
-    collection.find().toArray(function(err, docs){
-      console.log(docs);
-      callback;
-    });
-}
+server.route( [
+    // Get tour list
+    {
+        method: 'GET',
+        path: '/api/tours',
+        handler: function(request, reply) {
+          return "Getting tour list!";
+        }
+    },
+    // Add new tour
+    {
+        method: 'POST',
+        path: '/api/tours',
+        handler: function(request, reply) {
+          return "Adding new tour";
+        }
+    },
+    // Get a single tour
+    {
+        method: 'GET',
+        path: '/api/tours/{name}',
+        handler: function(request, reply) {
+          return "Retrieving " + request.params.name;
+        }
+    },
+    // Update a single tour
+    {
+        method: 'PUT',
+        path: '/api/tours/{name}',
+        handler: function(request, reply) {
+            // request.payload variables
+            return "Updating " + request.params.name;
+        }
+    },
+    // Delete a single tour
+    {
+        method: 'DELETE',
+        path: '/api/tours/{name}',
+        handler: function(request, reply) {
+          return "Deleting " + request.params.name;
+        }
+    },
+    // Home page
+    {
+        method: 'GET',
+        path: '/',
+        handler: function(request, reply) {
+          return "Hello world from Hapi/Mongo example.";
+        }
+    }
+]);
+
 MongoClient.connect(url, function(err, client){
   console.log("Connected successfully to server");
   var db = client.db('learning_mongo');
-  findDocuments(db, function(){
-    db.close();
+  collection = db.collection('tours');
+
+  server.start(function(err) {
+      console.log('Hapi is listening to http://localhost:8080')
   });
-})
+});
